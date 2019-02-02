54e876c3e356de7729d7fa804a03a1c269cc5b0e
Anton Alekseev <undesazar7@gmail.com>
2019-02-02 18:55:31 +0300

    Copy/paste comments tables.

--- laravel/database/migrations/2019_02_01_162000_create_comments_table.php
+++ laravel/database/migrations/2019_02_01_162000_create_comments_table.php
@@ -13,10 +13,40 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
+        /**  Build comments table */
         Schema::create('comments', function (Blueprint $table) {
+
             $table->increments('id');
+
+            $table->text('comment');
+
+            $table->integer('votes')->default(0);
+
+            $table->integer('spam')->default(0);
+
+            $table->integer('reply_id')->default(0);
+
+            $table->string('page_id')->default(0);
+
+            $table->integer('users_id');
+
             $table->timestamps();
+
         });
+
+        /**  Build comments_user_vote table */
+        Schema::create('comment_user_vote', function (Blueprint $table) {
+
+            $table->integer('comment_id');
+
+            $table->integer('user_id');
+
+            $table->string('vote',11);  /** Can use bool instead of string? But req look like (if qString) ?vote=1. Mb change in future */
+
+        });
+
+        /** TODO Table [UserSeenComment] in partial migration */
+        /** TODO ForeignKeys and Constraints migration in partial file */
     }
 
     /**
